d44282dbf9679cdccddf2c9dc254c44eab6e133b
Shaileee <41407377+Shaileee@users.noreply.github.com>
2025-02-02 18:13:02 +0530

    Delete News_Demo_App/companyinfo directory
    delete directory

--- News_Demo_App/companyinfo/src/main/java/shaileedemo/project/companyinfo/archmodel/NewsIntent.kt
+++
@@ -1,8 +0,0 @@
-package shaileedemo.project.companyinfo.archmodel
-
-import shaileedemo.project.common.mvibase.MviIntent
-
-sealed class NewsIntent : MviIntent{
-    data object InitialIntent : NewsIntent()
-    data object GetNextCompanyNews : NewsIntent()
-}
